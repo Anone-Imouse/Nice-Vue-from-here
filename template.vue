91bd825ebe1e21fd1d1cd5b74a5eafa8136484df
Anone-Imouse <73369548+Anone-Imouse@users.noreply.github.com>
2020-12-29 11:38:31 -0500

    Create template.vue

---
+++ template.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <h1>{{message}}</h1>
+<input @keyup.enter="add" type="text" v-model.trim="addinp" maxLength="15">
+    <br>
+    <br>
+    <button @click="add">Add Todo</button>
+    <button @click="clear">Clear List</button>
+    <ul>
+      <li v-for="todo in todos">{{todo}}<button @click="remove">Remove</button></li>
+    </ul>
+  </div>
+</template>
